80328f4a20be44432954c3477310a05f3cfcaa1a
zhangxiaobing <13823586491@139.com>
2015-05-09 23:14:28 +0800

    update

--- src/main/java/com/service/user/impl/UserServiceImpl.java
+++ src/main/java/com/service/user/impl/UserServiceImpl.java
@@ -1,11 +1,29 @@
 package com.service.user.impl;
 
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.entity.base.UserInfo;
+import com.dao.base.BaseDao;
+import com.entity.user.UserInfo;
 import com.service.base.impl.BaseServiceImpl;
 import com.service.user.UserService;
 @Service
 public class UserServiceImpl extends BaseServiceImpl<UserInfo> implements UserService{
-
+	@Autowired
+    private BaseDao<UserInfo> userInfoDao;
+	@Override
+	public UserInfo findUserInfoByPhone(String userPhone) {
+		String hql = "from UserInfo u where u.userPhone =:phone";
+		HashMap<String, Object> params = new HashMap<String, Object>();
+		params.put("phone", userPhone);
+		List<UserInfo> listUser = userInfoDao.find(hql, params, 1,1);
+		if(listUser != null){
+			return listUser.get(0);
+		}
+		return null;
+	}
+           
 }
