0633839887f2a45641790678d53eccb80b2f2feb
zzw <995025884@qq.com>
2015-05-10 16:20:42 +0800

    u by zzw

---
+++ src/main/java/com/service/user/UserService.java
@@ -0,0 +1,8 @@
+package com.service.user;
+
+import com.entity.user.UserInfo;
+import com.service.base.BaseService;
+
+public interface UserService extends BaseService<UserInfo>{
+
+}
---
+++ src/main/java/com/service/user/impl/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.service.user.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.entity.user.UserInfo;
+import com.service.base.impl.BaseServiceImpl;
+import com.service.user.UserService;
+@Service
+public class UserServiceImpl extends BaseServiceImpl<UserInfo> implements UserService{
+
+}
